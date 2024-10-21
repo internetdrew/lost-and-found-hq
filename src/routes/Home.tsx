a8848273a471e7796347103af5bc4947e7b6d311
Andrew Rowley <andrew@internetdrew.com>
2024-10-21 14:54:02 -0400

    add home route component

---
+++ src/routes/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className='flex flex-col mx-4 lg:mx-0'>
+      <header className='max-w-screen-xl mx-auto text-center mt-10'>
+        <h1 className='text-4xl font-bold'>Welcome to Lost & Found HQ</h1>
+        <h2 className='text-lg mt-2'>
+          Lost & Found HQ is a platform that helps companies manage lost and
+          found items.
+        </h2>
+      </header>
+      <main className='max-w-screen-xl mx-auto mt-10'>hello</main>
+    </div>
+  );
+};
+
+export default Home;
