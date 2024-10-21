f69f3ca4e4b0ffbe256869749e95646fa96f254e
Andrew Rowley <andrew@internetdrew.com>
2024-10-21 14:53:34 -0400

    add nav component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center py-4 px-8'>
+      <div className='text-xl font-semibold'>Lost & Found HQ</div>
+      <div>menu coming soon</div>
+    </nav>
+  );
+};
+
+export default Navbar;
