9f070b2db9a90f98eee21580633f1a080ddec878
Andrew Rowley <andrew@internetdrew.com>
2024-10-23 14:04:02 -0400

    testing for origin issues

--- server/api/index.ts
+++ server/api/index.ts
@@ -7,7 +7,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
