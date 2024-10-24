914f6e2f1bbd710c1fcd6b22ac381f41a90c51df
Andrew Rowley <95974309+internetdrew@users.noreply.github.com>
2024-10-24 00:03:58 -0400

    reduce redundancy of port assignment (#8)

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import { PORT } from './config';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +13,6 @@ app.get('/api', (req: Request, res: Response) => {
   res.send('Hello from Lost & Found HQ API!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { PORT } from './server/api/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
     },
