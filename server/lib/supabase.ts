1b968d91f8aa38f6b49b82642397dfee55fbb813
Andrew Rowley <andrew@internetdrew.com>
2024-10-25 14:02:49 -0400

    add supabase client creator

---
+++ server/lib/supabase.ts
@@ -0,0 +1,32 @@
+import {
+  createServerClient,
+  parseCookieHeader,
+  serializeCookieHeader,
+} from '@supabase/ssr';
+import { Request, Response } from 'express';
+
+export const createClient = (req: Request, res: Response) => {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+    throw new Error('Missing required environment variables for Supabase');
+  }
+
+  return createServerClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(req.headers.cookie ?? '');
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            res.appendHeader(
+              'Set-Cookie',
+              serializeCookieHeader(name, value, options)
+            )
+          );
+        },
+      },
+    }
+  );
+};
