6fcc8e99cec8dd2b073485f9aba802f45049cb45
Andrew Rowley <andrew@internetdrew.com>
2024-10-29 14:43:52 -0400

    adds tooltip to button when it is disabled

--- src/components/dashboard/AddNewItemButtonAndDialog.tsx
+++ src/components/dashboard/AddNewItemButtonAndDialog.tsx
@@ -8,13 +8,41 @@ import {
 } from '../ui/dialog';
 import { Button } from '../ui/button';
 import NewItemForm from '../forms/NewItemForm';
+import { useLocations } from '@/hooks/useLocations';
+import { Skeleton } from '../ui/skeleton';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '../ui/tooltip';
 
 const AddNewItemButtonAndDialog = () => {
+  const { data, isLoading } = useLocations();
+  const noLocations = data.length === 0;
+
+  if (isLoading) return <Skeleton className='w-20 h-10' />;
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button>Add new item</Button>
-      </DialogTrigger>
+      {noLocations ? (
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <Button disabled={noLocations}>Add new item</Button>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent side='top' align='center' sideOffset={5}>
+              <p>You need to add a location first</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        <DialogTrigger asChild>
+          <Button>Add new item</Button>
+        </DialogTrigger>
+      )}
       <DialogContent className='font-mono overflow-y-scroll max-h-screen'>
         <DialogHeader>
           <DialogTitle>Add Item</DialogTitle>
