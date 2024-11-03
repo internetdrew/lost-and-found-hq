3a1e1edb9c3e30c34b20ecf7845874503e8cff97
Andrew Rowley <andrew@internetdrew.com>
2024-11-03 13:02:44 -0500

    add hook for items

---
+++ src/hooks/useItems.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import useSWR from 'swr';
+import { Item } from '../../server/database.types';
+
+const fetcher = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    const data: Item[] = response.data;
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      return null;
+    }
+    throw error;
+  }
+};
+
+export const useItems = () => {
+  const { data, isLoading, mutate } = useSWR('api/v1/items', fetcher);
+
+  return {
+    data,
+    isLoading,
+    mutate,
+  };
+};
