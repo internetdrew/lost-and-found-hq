f453807f233b4ecc1d9bc68ad0d43515c57d27d5
Andrew Rowley <andrew@internetdrew.com>
2024-11-03 12:58:13 -0500

    move dashboard to routes directory

---
+++ src/routes/Dashboard.tsx
@@ -0,0 +1,34 @@
+import CompanyInfoCard from '@/components/CompanyInfoCard';
+import AddNewItemButtonAndDialog from '@/components/dashboard/AddNewItemButtonAndDialog';
+import ItemList from '@/components/dashboard/ItemList';
+
+export interface CompanyInfo {
+  name: string;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+const Dashboard = () => {
+  return (
+    <div className='px-4 sm:px-8'>
+      <main>
+        <header className='mt-6'>
+          <div className='flex justify-between items-center'>
+            <h1 className='text-lg'>Dashboard</h1>
+            <AddNewItemButtonAndDialog />
+          </div>
+          <p className='text-sm text-gray-600 max-w-md mt-2'>
+            Here's where you can manage all of the items lost and found at your
+            place of business.
+          </p>
+          <CompanyInfoCard />
+        </header>
+        <ItemList />
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
