0085bab99c3bf848be1397b0759c5cfc6007d129
Andrew Rowley <andrew@internetdrew.com>
2024-11-08 12:19:06 -0500

    make item form dialog independent component

---
+++ src/components/dashboard/dialogs/ItemFormDialog.tsx
@@ -0,0 +1,43 @@
+import { DialogDescription } from '@/components/ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '../../ui/dialog';
+import ItemInfoForm from '@/components/forms/ItemInfoForm';
+import { Tables } from '@dbTypes';
+
+interface ItemFormDialogProps {
+  item?: Tables<'items'> | null;
+  renderItemDialog: boolean;
+  setRenderItemDialog: (value: boolean) => void;
+}
+
+const ItemFormDialog = ({
+  item = null,
+  renderItemDialog,
+  setRenderItemDialog,
+}: ItemFormDialogProps) => {
+  const title = item ? 'Edit Item' : 'Add Item';
+  const description = item
+    ? "Enter details of the item you'd like to edit. Be sure to not include any personal information."
+    : "Enter details of the item you'd like to add. Be sure to not include any personal information.";
+
+  return (
+    <Dialog open={renderItemDialog} onOpenChange={setRenderItemDialog}>
+      <DialogContent className='font-mono overflow-y-scroll max-h-screen'>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <ItemInfoForm
+          initialData={item}
+          onSuccess={() => setRenderItemDialog(false)}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ItemFormDialog;
