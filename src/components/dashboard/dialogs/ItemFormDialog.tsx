d801aa393e3d7a821a9a072c6fa7f531d6ff4407
Andrew Rowley <andrew@internetdrew.com>
2024-11-09 08:39:59 -0500

    add 90% view height max

--- src/components/dashboard/dialogs/ItemFormDialog.tsx
+++ src/components/dashboard/dialogs/ItemFormDialog.tsx
@@ -26,7 +26,7 @@ const ItemFormDialog = ({
 
   return (
     <Dialog open={renderItemDialog} onOpenChange={setRenderItemDialog}>
-      <DialogContent className='font-mono overflow-y-scroll max-h-screen'>
+      <DialogContent className='font-mono max-h-[90vh] overflow-y-scroll'>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
