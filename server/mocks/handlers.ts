9efbc4b01335be705d591daafd1874f5f1616504
Andrew Rowley <andrew@internetdrew.com>
2024-11-10 08:25:17 -0500

    set up mock server

---
+++ server/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('/auth/user', () => {
+    return HttpResponse.json({
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
+    });
+  }),
+  http.post('/auth/login', () => {
+    console.log('Captured a "POST /auth/login" request');
+    return HttpResponse.json({
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
+    });
+  }),
+];
