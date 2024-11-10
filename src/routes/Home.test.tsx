a9e837b1c8f59dd3e4ed45ca1f647ae99cee23f1
Andrew Rowley <andrew@internetdrew.com>
2024-11-10 08:25:32 -0500

    improve testing for home page

--- src/routes/Home.test.tsx
+++ src/routes/Home.test.tsx
@@ -1,10 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
+import { beforeAll, afterEach, afterAll } from 'vitest';
 
 import Home from './Home';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import App from '@/App';
+import { server } from '@server/mocks/node';
+import { http } from 'msw';
+import { HttpResponse } from 'msw';
+
+const mockNavigate = vi.fn();
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: () => mockNavigate,
+  };
+});
+
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  mockNavigate.mockReset();
+});
+afterAll(() => server.close());
 
 const renderHome = () => {
   return render(
@@ -76,5 +96,30 @@ describe('Home', () => {
 
     expect(screen.getByText(emailValidationText)).toBeInTheDocument();
     expect(screen.getByText(passwordValidationText)).toBeInTheDocument();
+  });
+
+  it('successfully logs users in and redirects to the dashboard', async () => {
+    const user = userEvent.setup();
+    server.use(
+      http.post('/auth/login', () => {
+        return HttpResponse.json({
+          message: 'Login successful',
+          redirectTo: '/dashboard',
+        });
+      })
+    );
+    renderHome();
+
+    const emailInput = screen.getByLabelText('Email address');
+    const passwordInput = screen.getByLabelText('Password');
+    const loginButton = screen.getByRole('button', { name: 'Login' });
+
+    await user.type(emailInput, 'test@test.com');
+    await user.type(passwordInput, 'password');
+    await user.click(loginButton);
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith('/dashboard');
+    });
   });
 });
