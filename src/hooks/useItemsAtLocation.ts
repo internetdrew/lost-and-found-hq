422b5c7f5ca28e2216416fa02fcedb7e442fb0d6
Andrew Rowley <andrew@internetdrew.com>
2024-11-12 13:54:46 -0500

    add new query hook

---
+++ src/hooks/useItemsAtLocation.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import useSWR from 'swr';
+import { Tables } from '@dbTypes';
+
+type Item = Tables<'items'>;
+
+const fetcher = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    const items: Item[] = response.data;
+    return items;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      return null;
+    }
+    throw error;
+  }
+};
+
+export const useItemsAtLocation = (locationId: number) => {
+  const { data, isLoading, mutate } = useSWR(
+    `api/v1/locations/${locationId}/items`,
+    fetcher
+  );
+
+  return {
+    data,
+    isLoading,
+    mutate,
+  };
+};
