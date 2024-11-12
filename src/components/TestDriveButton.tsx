66d444514fa690f5c468fdb2d050e4b1bd0dcfb3
Andrew Rowley <andrew@internetdrew.com>
2024-11-12 17:02:42 -0500

    navigate to dashboard from test drive login

--- src/components/TestDriveButton.tsx
+++ src/components/TestDriveButton.tsx
@@ -1,12 +1,21 @@
 import { toast } from 'react-hot-toast';
 import { Button } from './ui/button';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSWRConfig } from 'swr';
 
 const TestDriveButton = () => {
+  const navigate = useNavigate();
+  const { mutate } = useSWRConfig();
   const handleTestDrive = async () => {
     try {
-      await axios.post('/auth/start-test-drive');
+      const res = await axios.post('/auth/start-test-drive');
       toast.success("Let's go for a spin!");
+      setTimeout(() => {
+        mutate('/auth/user');
+        navigate(res.data.redirectTo);
+      }, 1000);
+      return;
     } catch (error) {
       console.error(error);
       toast.error('Something went wrong. Please try again.');
