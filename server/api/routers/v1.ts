2999bca92f2fddc856c3bee4498d79bfa64beceb
Andrew Rowley <andrew@internetdrew.com>
2024-11-12 17:16:46 -0500

    add test drive controller and update item endpoints

--- server/api/routers/v1.ts
+++ server/api/routers/v1.ts
@@ -24,12 +24,15 @@ v1Router.post('/locations', addLocation);
 v1Router.put('/locations/:id', updateLocation);
 v1Router.delete('/locations/:id', deleteLocation);
 
-/* Items */
-v1Router.get('/items', getItems);
-v1Router.get('/items/:id', getItem);
-v1Router.post('/items', addItem);
-v1Router.put('/items/:id', updateItem);
-v1Router.patch('/items/:id/status', toggleItemActiveStatus);
-v1Router.delete('/items/:id', deleteItem);
+/* Location's Items */
+v1Router.get('/locations/:locationId/items', getItems);
+v1Router.get('/locations/:locationId/items/:id', getItem);
+v1Router.post('/locations/:locationId/items', addItem);
+v1Router.put('/locations/:locationId/items/:id', updateItem);
+v1Router.patch(
+  '/locations/:locationId/items/:id/status',
+  toggleItemActiveStatus
+);
+v1Router.delete('/locations/:locationId/items/:id', deleteItem);
 
 export default v1Router;
