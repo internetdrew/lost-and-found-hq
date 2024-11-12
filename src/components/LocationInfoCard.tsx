361e8ae38a3cea8c37f89b34bd816c6107d3f91e
Andrew Rowley <andrew@internetdrew.com>
2024-11-12 12:48:44 -0500

    render modal with correct form and data

--- src/components/LocationInfoCard.tsx
+++ src/components/LocationInfoCard.tsx
@@ -1,57 +1,55 @@
-import LocationInfoForm from './forms/LocationInfoForm';
 import { Button } from './ui/button';
-import {
-  Dialog,
-  DialogDescription,
-  DialogHeader,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-} from './ui/dialog';
-import { useLocations } from '@/hooks/useLocations';
-import { Skeleton } from './ui/skeleton';
+import LocationFormDialog from './dashboard/dialogs/LocationFormDialog';
+import { useState } from 'react';
+import { Tables } from '@dbTypes';
 
-export default function LocationInfoCard() {
-  const { data: locations, isLoading } = useLocations();
+type Location = Tables<'locations'>;
 
-  if (isLoading) return <Skeleton className='max-w-sm h-16 mt-4' />;
+export default function LocationInfoCard({
+  location,
+}: {
+  location: Location | null;
+}) {
+  const [renderLocationDialog, setRenderLocationDialog] = useState(false);
 
   return (
     <div className='flex items-start gap-4 mt-4 ring-1 ring-gray-200 p-4 rounded-md max-w-sm'>
-      {locations?.length > 0 ? (
-        <div>
-          <p className='text-sm font-semibold'>Location Info</p>
-          <p className='text-sm mt-4'>{locations[0].name}</p>
-          <p className='text-sm'>
-            {locations[0].address}
-            <br />
-            {locations[0].city}, {locations[0].state} {locations[0].postal_code}
-          </p>
-        </div>
+      {location ? (
+        <>
+          <div>
+            <p className='text-sm font-semibold'>Location Info</p>
+            <p className='text-sm mt-4'>{location.name}</p>
+            <p className='text-sm'>
+              {location.address}
+              <br />
+              {location.city}, {location.state} {location.postal_code}
+            </p>
+          </div>
+          <Button
+            variant='outline'
+            className='text-xs w-max ml-auto'
+            onClick={() => setRenderLocationDialog(true)}
+          >
+            Edit
+          </Button>
+        </>
       ) : (
-        <p className='text-sm'>There's no business location info yet.</p>
+        <>
+          <p className='text-sm'>There's no business location info yet.</p>
+          <Button
+            className='text-xs w-max ml-auto'
+            onClick={() => setRenderLocationDialog(true)}
+          >
+            Add
+          </Button>
+        </>
       )}
-      <Dialog>
-        <DialogTrigger asChild>
-          {locations?.length > 0 ? (
-            <Button variant='outline' className='text-xs w-max ml-auto'>
-              Edit
-            </Button>
-          ) : (
-            <Button className='text-xs w-max ml-auto'>Add</Button>
-          )}
-        </DialogTrigger>
-        <DialogContent className='font-mono overflow-y-scroll max-h-screen'>
-          <DialogHeader>
-            <DialogTitle>Business Location</DialogTitle>
-            <DialogDescription>
-              Before you can add items, we'll need some details about your
-              business location.
-            </DialogDescription>
-          </DialogHeader>
-          <LocationInfoForm />
-        </DialogContent>
-      </Dialog>
+
+      <LocationFormDialog
+        location={location}
+        renderLocationDialog={renderLocationDialog}
+        setRenderLocationDialog={setRenderLocationDialog}
+      />
     </div>
   );
 }
