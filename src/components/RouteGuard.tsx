d91d24f22c0d45195fcc5b6e65b31855bac8fdab
Andrew Rowley <andrew@internetdrew.com>
2024-11-21 09:40:11 -0500

    improve semantics of route guard

--- src/components/RouteGuard.tsx
+++ src/components/RouteGuard.tsx
@@ -6,22 +6,30 @@ import Spinner from './Spinner';
 interface RouteGuardProps {
   children: React.ReactNode;
   requiresAuth: boolean;
-  redirectTo?: string;
+  rejectAuthUser?: boolean;
 }
 
 const RouteGuard = ({
   children,
   requiresAuth,
-  redirectTo = requiresAuth ? '/' : '/dashboard',
+  rejectAuthUser,
 }: RouteGuardProps) => {
   const navigate = useNavigate();
   const { data: user, isLoading } = useUser();
 
   useEffect(() => {
-    if (!isLoading && (requiresAuth ? !user : user)) {
-      navigate(redirectTo, { replace: true });
+    if (isLoading) return;
+
+    if (requiresAuth && !user) {
+      navigate('/', { replace: true });
+      return;
     }
-  }, [user, isLoading, navigate, requiresAuth, redirectTo]);
+
+    if (!requiresAuth && user && rejectAuthUser) {
+      navigate('/dashboard', { replace: true });
+      return;
+    }
+  }, [user, isLoading, navigate, requiresAuth, rejectAuthUser]);
 
   if (isLoading) {
     return <Spinner />;
