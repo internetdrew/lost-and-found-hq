df5e4d0264acaaee06769f162748e68f352783ae
Andrew Rowley <andrew@internetdrew.com>
2024-11-21 14:05:19 -0500

    add new hook for getting location info

---
+++ src/hooks/useLocationInfo.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import useSWR from 'swr';
+import { Tables } from '@dbTypes';
+
+type Location = Tables<'locations'>;
+
+const fetcher = async (url: string) => {
+  try {
+    const response = await axios.get(url);
+    const location: Location = response.data;
+    return location;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      return null;
+    }
+    throw error;
+  }
+};
+
+export const useLocationInfo = (locationId: string) => {
+  const { data, isLoading, mutate } = useSWR(
+    `/api/v1/locations/${locationId}`,
+    fetcher
+  );
+
+  return {
+    locationInfo: data,
+    isLoading,
+    mutate,
+  };
+};
