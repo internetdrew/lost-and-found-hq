07e49d3f77248a4b7e8c6fecdd0a01d01f902c93
Andrew Rowley <95974309+internetdrew@users.noreply.github.com>
2025-01-17 11:43:00 -0500

    Make Navbar Sticky (#30)
    * make navbar sticky
    
    * add pr link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className='flex justify-between items-center p-4 sm:px-8'>
+    <nav className='sticky top-0 flex justify-between items-center p-4 sm:px-8'>
       <a href='/' className='font-semibold text-lg sm:text-xl'>
         Lost & Found HQ
       </a>
