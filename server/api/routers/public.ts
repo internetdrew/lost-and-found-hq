6a322d0e0000e8968c57d8364c0e71538040dbd0
Andrew Rowley <andrew@internetdrew.com>
2025-01-17 10:33:52 -0500

    add new handlers for public items

--- server/api/routers/public.ts
+++ server/api/routers/public.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { addInterestedEmail } from '../controllers/interestController';
-import { resetTestUserItems } from '../controllers/itemsController';
+import {
+  resetTestUserItems,
+  getPublicItems,
+  getPublicItem,
+} from '../controllers/itemsController';
 
 const publicRouter = express.Router();
 
 publicRouter.post('/interest', addInterestedEmail);
 publicRouter.get('/cron/reset-test-items', resetTestUserItems);
+publicRouter.get('/locations/:locationId/items', getPublicItems);
+publicRouter.get('/locations/:locationId/items/:itemId', getPublicItem);
 
 export default publicRouter;
