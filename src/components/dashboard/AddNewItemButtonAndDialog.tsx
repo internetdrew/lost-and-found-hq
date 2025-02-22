580a71b5dea53cd8130b79aa95fb3e59ac1e50b7
Andrew Rowley <andrew@internetdrew.com>
2025-02-22 08:31:50 -0500

    update hook usage

--- src/components/dashboard/AddNewItemButtonAndDialog.tsx
+++ src/components/dashboard/AddNewItemButtonAndDialog.tsx
@@ -1,13 +1,20 @@
 import { Button } from '../ui/button';
 import { useState } from 'react';
 import ItemFormDialog from './dialogs/ItemFormDialog';
+import { useLocations } from '@/hooks/useData';
 
 const AddNewItemButtonAndDialog = () => {
+  const { locations, isLoading } = useLocations();
   const [renderItemDialog, setRenderItemDialog] = useState(false);
 
   return (
     <>
-      <Button onClick={() => setRenderItemDialog(true)}>Add item</Button>
+      <Button
+        disabled={isLoading || !locations?.[0]}
+        onClick={() => setRenderItemDialog(true)}
+      >
+        Add item
+      </Button>
       <ItemFormDialog
         renderItemDialog={renderItemDialog}
         setRenderItemDialog={setRenderItemDialog}
